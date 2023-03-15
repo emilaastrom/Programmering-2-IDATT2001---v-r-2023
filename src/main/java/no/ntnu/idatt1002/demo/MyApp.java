2a4fe3bf8df81b19288c39889b1601f8ad1c277a
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-03-15 09:50:23 +0100

    Changing template to use JavaFX instead of Swing

--- src/main/java/no/ntnu/idatt1002/demo/MyApp.java
+++ src/main/java/no/ntnu/idatt1002/demo/MyApp.java
@@ -1,18 +1,25 @@
 package no.ntnu.idatt1002.demo;
 
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import no.ntnu.idatt1002.demo.view.MyWindow;
 
 /**
  * Use this class to start the application
  * @author nilstes
  */
-public class MyApp {
+public class MyApp extends Application {
 
     /**
      * Main method for my application
      */
     public static void main(String[] args) throws Exception {
-        MyWindow window = new MyWindow("The Window");
-        window.setVisible(true);
-   }  
+        launch();
+   }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+    }
 }
