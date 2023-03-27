01f679a26f6448ef9088b9b201cd1ce0c62ab139
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-03-27 12:51:58 +0200

    Initial unit tests

---
+++ src/test/java/no/ntnu/idatt1002/demo/data/BudgetTest.java
@@ -0,0 +1,87 @@
+package no.ntnu.idatt1002.demo.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BudgetTest {
+
+    Budget testBudget;
+
+    @BeforeEach
+    void setUp() {
+        testBudget = new Budget("testUser");
+        testBudget.addIncome("Jobb", 25000);
+        testBudget.addIncome("Ekstrajobb", 10000);
+        testBudget.addExpense("Mat", 5000);
+        testBudget.addExpense("Bolig", 10000);
+        testBudget.addExpense("Transport", 5000);
+    }
+
+    @Test
+    void addIncome() {
+        testBudget.addIncome("TestLønn", 25000);
+        assertEquals(25000, testBudget.getIncomeValue("TestLønn"));
+
+        testBudget.addIncome("TestLønn2", 25000);
+        assertNotEquals(300, testBudget.getIncomeValue("TestLønn2"));
+    }
+
+    @Test
+    void addExpense() {
+        testBudget.addExpense("TestUtgift", 25000);
+        assertEquals(25000, testBudget.getExpenseValue("TestUtgift"));
+
+        testBudget.addExpense("TestUtgift2", 25000);
+        assertNotEquals(300, testBudget.getExpenseValue("TestUtgift2"));
+    }
+
+    @Test
+    void removeIncome() {
+        assertEquals(25000, testBudget.getIncomeValue("Jobb"));
+
+        testBudget.removeIncome("Jobb");
+        assertNotEquals(25000, testBudget.getIncomeValue("Ekstrajobb"));
+
+        // Forventer -1 som betyr at inntekten ikke finnes
+        testBudget.removeIncome("Ekstrajobb");
+        assertEquals((-1), testBudget.getIncomeValue("Ekstrajobb"));
+    }
+
+    @Test
+    void removeExpense() {
+    }
+
+    @Test
+    void editIncome() {
+    }
+
+    @Test
+    void editExpense() {
+    }
+
+    @Test
+    void getExpenseValue() {
+    }
+
+    @Test
+    void getIncomeValue() {
+    }
+
+    @Test
+    void getTotalIncome() {
+    }
+
+    @Test
+    void getTotalExpense() {
+    }
+
+    @Test
+    void getIncomeList() {
+    }
+
+    @Test
+    void getExpenseList() {
+    }
+}
