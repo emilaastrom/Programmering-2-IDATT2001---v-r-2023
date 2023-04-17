4aed5b148a828c659e31d11aeb49c398e866ec23
Kenny Tran <kbtran@stud.ntnu.no>
2023-04-17 17:24:27 +0200

    finished tests for Budget class

--- src/test/java/no/ntnu/idatt1002/demo/data/BudgetTest.java
+++ src/test/java/no/ntnu/idatt1002/demo/data/BudgetTest.java
@@ -3,6 +3,8 @@ package no.ntnu.idatt1002.demo.data;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BudgetTest {
@@ -26,6 +28,8 @@ class BudgetTest {
 
         testBudget.addIncome("TestLønn2", 25000);
         assertNotEquals(300, testBudget.getIncomeValue("TestLønn2"));
+
+        assertEquals(testBudget.getIncomeList().size(), 4);
     }
 
     @Test
@@ -35,6 +39,8 @@ class BudgetTest {
 
         testBudget.addExpense("TestUtgift2", 25000);
         assertNotEquals(300, testBudget.getExpenseValue("TestUtgift2"));
+
+        assertEquals(testBudget.getExpenseList().size(), 5);
     }
 
     @Test
@@ -60,33 +66,87 @@ class BudgetTest {
 
     @Test
     void editIncome() {
+        testBudget.editIncome("Jobb", 100);
+        assertEquals(100, testBudget.getIncomeValue("Jobb"));
     }
 
     @Test
     void editExpense() {
+        testBudget.editExpense("Mat", 200);
+        assertEquals(200, testBudget.getExpenseValue("Mat"));
     }
 
     @Test
     void getExpenseValue() {
+        assertEquals(10000, testBudget.getExpenseValue("Bolig"));
     }
 
     @Test
     void getIncomeValue() {
+        assertEquals(10000, testBudget.getIncomeValue("Ekstrajobb"));
     }
 
     @Test
     void getTotalIncome() {
+        assertEquals(35000, testBudget.getTotalIncome());
+
+        testBudget.addIncome("Testjobb", 4000);
+        testBudget.removeIncome("Ekstrajobb");
+        testBudget.editIncome("Testjobb", 200);
+
+        assertEquals(25200, testBudget.getTotalIncome());
     }
 
     @Test
     void getTotalExpense() {
+        assertEquals(20000, testBudget.getTotalExpense());
+
+        testBudget.addExpense("Testkostnader", 2000);
+        testBudget.removeExpense("Bolig");
+        testBudget.editExpense("Testkostnader", 0);
+
+        assertEquals(10000, testBudget.getTotalExpense());
     }
 
     @Test
     void getIncomeList() {
+        assertEquals(2, testBudget.getIncomeList().size());
+        assertEquals(3, testBudget.getExpenseList().size());
+
+        ArrayList<String> incomeNames = new ArrayList<>();
+        ArrayList<Double> incomeValues = new ArrayList<>();
+
+        incomeNames.add("Jobb");
+        incomeNames.add("Ekstrajobb");
+
+        incomeValues.add(25000.0);
+        incomeValues.add(10000.0);
+
+        for(int i = 0; i < testBudget.getIncomeList().size(); i++) {
+            assertEquals(incomeNames.get(i), testBudget.getIncomeList().get(i).getBudgetItemName());
+            assertEquals(incomeValues.get(i), testBudget.getIncomeList().get(i).getBudgetItemValue());
+        }
+
     }
 
     @Test
     void getExpenseList() {
+        assertEquals(3, testBudget.getExpenseList().size());
+
+        ArrayList<String> expenseNames = new ArrayList<>();
+        ArrayList<Double> expenseValues = new ArrayList<>();
+
+        expenseNames.add("Mat");
+        expenseNames.add("Bolig");
+        expenseNames.add("Transport");
+
+        expenseValues.add(5000.0);
+        expenseValues.add(10000.0);
+        expenseValues.add(5000.0);
+
+        for(int i = 0; i < testBudget.getExpenseList().size(); i++) {
+            assertEquals(expenseNames.get(i), testBudget.getExpenseList().get(i).getBudgetItemName());
+            assertEquals(expenseValues.get(i), testBudget.getExpenseList().get(i).getBudgetItemValue());
+        }
     }
 }
