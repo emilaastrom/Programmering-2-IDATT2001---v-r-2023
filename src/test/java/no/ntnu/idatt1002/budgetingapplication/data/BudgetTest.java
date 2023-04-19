a31b45aa9d20bdd3e7cd164d08e9788b1619b450
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-04-19 12:25:39 +0200

    Cleaned up and restructured project directory

--- src/test/java/no/ntnu/idatt1002/budgetingapplication/data/BudgetTest.java
+++ src/test/java/no/ntnu/idatt1002/budgetingapplication/data/BudgetTest.java
@@ -2,9 +2,7 @@ package no.ntnu.idatt1002.budgetingapplication.data;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BudgetTest {
