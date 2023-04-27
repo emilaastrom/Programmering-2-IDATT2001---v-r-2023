9f2670b17f5d4d36b7f9c452621aa63cd4aa38e9
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-04-27 14:48:53 +0200

    Updated JavaDoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"no.ntnu.idatt1002.demo"},{"l":"no.ntnu.idatt1002.demo.data"},{"l":"no.ntnu.idatt1002.demo.repo"},{"l":"no.ntnu.idatt1002.demo.view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"no.ntnu.idatt1002.budgetingapplication"},{"l":"no.ntnu.idatt1002.budgetingapplication.data"}];updateSearchResults();
